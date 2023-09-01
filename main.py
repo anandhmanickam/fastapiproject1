99cc24257c3bc6c35747a9022d4fe54943dba07f
anandhmanickam <anandhmanickam65@gmail.com>
2023-09-01 12:46:55 +0530

    file chnaged

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI, Depends,  HTTPException
-from database import SessionLocal,engine,Base
+from fastapi import FastAPI, Depends, HTTPException
+from database import SessionLocal, engine, Base
 from models import Users
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
@@ -10,6 +10,7 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+
 def get_db():
     db = SessionLocal()
     try:
@@ -17,53 +18,53 @@ def get_db():
     finally:
         db.close()
 
+
 class UserSchema(BaseModel):
-    id: int 
-    name : str
+    id: int
+    name: str
     email: str
+
     class Config:
         orm_mode = True
-        
+
+
 class UserCreateSchemae(UserSchema):
-    age : int
-    job : str  
-        
-        
-@app.get('/users', response_model=list[UserCreateSchemae]) 
+    age: int
+    job: str
+
+
+@app.get("/users", response_model=list[UserCreateSchemae])
 def get_user(db: Session = Depends(get_db)):
     return db.query(Users).all()
 
 
-@app.post('/users', response_model=UserSchema) 
-def get_user(user:UserCreateSchemae, db: Session = Depends(get_db)):
-    
-    db_use = Users(name = user.name, age = user.age, email = user.email, job = user.job)
+@app.post("/users", response_model=UserSchema)
+def get_user(user: UserCreateSchemae, db: Session = Depends(get_db)):
+    db_use = Users(name=user.name, age=user.age, email=user.email, job=user.job)
     db.add(db_use)
     db.commit()
     return db_use
 
 
-@app.put('/users/{user_id}',response_model=UserSchema)
-def update_user(user_id: int, user:UserSchema, db: Session = Depends(get_db)):
+@app.put("/users/{user_id}", response_model=UserSchema)
+def update_user(user_id: int, user: UserSchema, db: Session = Depends(get_db)):
     try:
-        db_use = db.query(Users).filter(Users.id==user_id).first()
+        db_use = db.query(Users).filter(Users.id == user_id).first()
         db_use.name = user.name
         db_use.email = user.email
         db.add(db_use)
         db.commit()
         return db_use
     except:
-        return HTTPException(status_code=404, detail="User not found")
+        return HTTPException(status_code=404, detail="User not found.")
 
-        
-        
-@app.delete('/users/{user_id}', response_class=JSONResponse)
+
+@app.delete("/users/{user_id}", response_class=JSONResponse)
 def delete_user(user_id: int, db: Session = Depends(get_db)):
     try:
-        db_use = db.query(Users).filter(Users.id==user_id).first()
+        db_use = db.query(Users).filter(Users.id == user_id).first()
         db.delete(db_use)
         db.commit()
-        return{f'user id{user_id} deleted':True}
+        return {f"user id{user_id} deleted": True}
     except:
         return HTTPException(status_code=404, detail="User not found")
-        
